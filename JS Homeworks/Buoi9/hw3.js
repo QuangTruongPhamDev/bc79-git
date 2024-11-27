a3eddd24eb1199225b0bea729ae09f13c1306ae2
QuangTruongPhamDev <quangtruongpham011@gmail.com>
2024-11-27 15:12:38 +0700

    JS Buoi9

---
+++ JS Homeworks/Buoi9/hw3.js
@@ -0,0 +1,14 @@
+/* Đầu vào 
+- Khai báo các biến usd là số usd cần quy đổi, dong là giá trị viet nam dong của 1 usd, money là kết quả số tiền sau khi quy đổi
+* Các bước thực hiện
+- Khai báo và gán giá trị các biến usd , dong
+- Khai báo và gán giá trị biến money là phép tính nhân giữa usd và dong
+- Thực hiện phép tính nhân
+* Đầu ra
+- Trả về kết quả của biến money.
+**/
+
+let usd = 12;
+let dong = 23500;
+let money = usd * dong;
+console.log("Số tiền quy đổi:", money);
